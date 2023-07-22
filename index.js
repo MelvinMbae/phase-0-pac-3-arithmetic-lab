5948bb8e482ca4b3834f5ba1c5f4c167a2b338f8
Melvin Mbae <melvinmuthomi95@gmail.com>
2023-07-22 11:05:54 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a, b) {
+    return a + b;
+}
+add(2, 2)
+
+function subtract(a, b) {
+    return a - b;
+}
+subtract(4, 1)
+
+function multiply(a, b) {
+    return a * b;
+}
+multiply(2, 2)
+
+function divide(a, b) {
+    return a / b;
+}
+divide(4, 2)
+
+function increment(n) {
+    return n += 1;
+}
+console.log(increment(1))
+
+function decrement(n) {
+    return n -= 1;
+}
+console.log(decrement(2))
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+console.log(makeInt("5.333"))
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+console.log(makeInt("JustAString"))
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+console.log(preserveDecimal("5.333"))
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+console.log(preserveDecimal("JustAString"))
